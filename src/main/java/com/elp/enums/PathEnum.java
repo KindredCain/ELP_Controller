5ccd174a2fa58f59f5523ab56bdb5612b6469ae6
Kindred <hznathan@gmail.com>
2017-07-05 15:56:40 +0800

    添加推荐算法，添加路径枚举
    添加路径枚举
    添加pagerank推荐算法

---
+++ src/main/java/com/elp/enums/PathEnum.java
@@ -0,0 +1,28 @@
+package com.elp.enums;
+
+import java.io.File;
+
+/**
+ * Created by NWJ on 2017/7/5.
+ */
+public enum PathEnum {
+    MAP_PATH("." + File.separator
+            + "main" + File.separator
+            + "resources" + File.separator
+            + "Item" + File.separator
+            + "mapsave"),;
+
+    private String path;
+
+    PathEnum(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
