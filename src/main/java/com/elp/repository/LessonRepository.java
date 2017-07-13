6da7dd50fc534db0ae634b3cb6113277e330ecf7
simpleag <1013194733@qq.com>
2017-07-13 12:17:56 +0800

    修正BUG

--- src/main/java/com/elp/repository/LessonRepository.java
+++ src/main/java/com/elp/repository/LessonRepository.java
@@ -39,18 +39,18 @@ public interface LessonRepository extends JpaRepository<Lesson,String> {
             "tb_lesson.del_time lesdeltime, " +
             "tb_lesson.update_time lesuptime, " +
             "tb_lesson.course_num, " +
-            "tb_lesson.expect_complete lesexp, " +
-            "tb_lesson.lesson_info, " +
             "tb_lesson.lesson_name, " +
+            "tb_lesson.lesson_info, " +
+            "tb_lesson.expect_complete lesexp, " +
             "tb_lesson.lesson_type, " +
             "tb_lesson.lesson_order, " +
             "tb_lessonrecord.object_id lesrecid, " +
             "tb_lessonrecord.creat_time lesreccretime, " +
             "tb_lessonrecord.del_time lesrecdeltime, " +
             "tb_lessonrecord.update_time lesrecuptime, " +
+            "tb_lessonrecord.user_num, " +
             "tb_lessonrecord.lesson_num, " +
             "tb_lessonrecord.lesson_record, " +
-            "tb_lessonrecord.user_num, " +
             "tb_course.object_id couid, " +
             "tb_course.creat_time coucretime, " +
             "tb_course.del_time coudeltime, " +
@@ -61,7 +61,7 @@ public interface LessonRepository extends JpaRepository<Lesson,String> {
             "tb_course.course_sum_lesson, " +
             "tb_course.expect_complete couexp, " +
             "tb_course.course_power, " +
-            "tb_course.course_info " +
+            "tb_course.course_info, " +
             "tb_course.course_pic_url " +
             "FROM tb_lesson LEFT JOIN tb_lessonrecord " +
             "ON (tb_lesson.object_id = tb_lessonrecord.lesson_num " +
@@ -78,29 +78,30 @@ public interface LessonRepository extends JpaRepository<Lesson,String> {
             "tb_lesson.del_time lesdeltime, " +
             "tb_lesson.update_time lesuptime, " +
             "tb_lesson.course_num, " +
-            "tb_lesson.expect_complete lesexp, " +
-            "tb_lesson.lesson_info, " +
             "tb_lesson.lesson_name, " +
+            "tb_lesson.lesson_info, " +
+            "tb_lesson.expect_complete lesexp, " +
             "tb_lesson.lesson_type, " +
             "tb_lesson.lesson_order, " +
             "tb_lessonrecord.object_id lesrecid, " +
             "tb_lessonrecord.creat_time lesreccretime, " +
             "tb_lessonrecord.del_time lesrecdeltime, " +
             "tb_lessonrecord.update_time lesrecuptime, " +
+            "tb_lessonrecord.user_num, " +
             "tb_lessonrecord.lesson_num, " +
             "tb_lessonrecord.lesson_record, " +
-            "tb_lessonrecord.user_num, " +
             "tb_course.object_id couid, " +
             "tb_course.creat_time coucretime, " +
             "tb_course.del_time coudeltime, " +
             "tb_course.update_time couuptime, " +
             "tb_course.admin_num, " +
             "tb_course.course_name, " +
-            "tb_course.course_power, " +
-            "tb_course.course_sum_lesson, " +
             "tb_course.course_url, " +
+            "tb_course.course_sum_lesson, " +
             "tb_course.expect_complete couexp, " +
-            "tb_course.course_info " +
+            "tb_course.course_power, " +
+            "tb_course.course_info, " +
+            "tb_course.course_pic_url " +
             "FROM tb_lesson LEFT JOIN tb_lessonrecord " +
             "ON (tb_lesson.object_id = tb_lessonrecord.lesson_num " +
             "AND tb_lessonrecord.del_time IS NULL " +
